f1c764e3dc0ea71c099a5509cd16c8ca22ee62b1
SkSaifuddin1998 <sksaifuddin575@gmail.com>
2025-08-05 23:58:12 +0530

    Docker Configartion

--- src/main/java/com/docker/saif/rest/DockerRestController.java
+++ src/main/java/com/docker/saif/rest/DockerRestController.java
@@ -24,6 +24,6 @@ public class DockerRestController {
 	
 	@GetMapping("/cicd")
 	public String dockerCICDPipeline() {
-		return "Welcome To Docker CICD pIPELINE add password ";
+		return "Welcome To Docker CICD pIPELINE add password Amin ";
 	}
 }
